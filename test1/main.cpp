18d1da47f68802020396625b1b077d9f5ba86a54
s1120406-lab <s1120406@dmhs.kh.edu.tw>
2025-10-16 14:29:14 +0800

    hw5

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,5 +1,26 @@
-     #include <iostream>
- int main() {
-   std::cout << "Hello, World!\n"; // 螢幕印出字串，\n 是換行
-   return 0;                        // 回傳 0 代表「程式順利結束」
- }
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 九九乘法表
+    cout << "=== 九九乘法表 ===" << endl;
+    for (int i = 1; i <= 9; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << endl; // 空一行作為區隔
+
+    // 星號金字塔
+    cout << "=== 星號金字塔 ===" << endl;
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
